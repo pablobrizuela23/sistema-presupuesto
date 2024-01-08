79654f3b0b55282d88b6128e71660d7ba8564cf5
pablobrizuela23 <latrell_302@hotmail.com>
2024-01-08 14:20:42 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const ingresos = [
+    new Ingreso("Salario",2100.00),
+    new Ingreso("venta coche",190800.00)
+];
+
+const egresos = [
+    new Egreso("Renta departamento",95000.00),
+    new Egreso("Ropa",400)
+];
+
+let cargarApp = ()=>{
+    cargarCabecero();
+}
+let totalIngresos = () => {
+    let totalIngreso = 0;
+    for (let ingreso of ingresos) {
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = ()=>{
+    let totalEgreso = 0;
+    for(let egreso of egresos){
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
+let cargarCabecero = () => {
+    let presupuesto = totalIngresos()- totalEgresos();
+    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    document.getElementById("presupuesto").innerHTML= formatoMoneda(presupuesto);
+    document.getElementById("porcentaje").innerHTML=formatoPorcentaje(porcentajeEgreso);
+    document.getElementById("ingresos").innerHTML= formatoMoneda(totalIngresos());
+    document.getElementById("egresos").innerHTML=formatoMoneda(totalEgresos());
+
+}
+const formatoMoneda = (valor)=>{
+    return valor.toLocaleString("en-US",{style:"currency",currency:"USD",minimumFractionDigits:2});
+}
+
+const formatoPorcentaje = () =>{
+    return valor.toLocaleString("en-US",{style:"percent",minumumFractionDigits:2});
+}
